756e088d2b5376e731ad5f348ac0facfb0fc6250
andrii.shaforostov <andpr99@gmail.com>
2020-08-08 21:58:02 +0300

    client_26

--- run.sh
+++ run.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-docker run --cap-add=NET_ADMIN \
--v openvpn_conf:/opt/Dockovpn \
--p 1194:1194/udp -p 80:8080/tcp \
--e HOST_ADDR=localhost \
+build -t skr2/openvpn-server-26 .
+
+docker run --name openvpn-26 --cap-add=NET_ADMIN \
+-d \
+-p 1195:1194/udp -p 80:8080/tcp \
+-e CLIENT_ID=client-26-db \
+-e HOST_ADDR=$(curl -s https://api.ipify.org) \
+-v openvpn-26:/opt/Dockovpn_data \
 --rm \
-alekslitvinenk/openvpn "$@"
+skr2/openvpn-server-26
+
+docker logs openvpn-26 -f
+
+#docker exec openvpn-26 ./genclient.sh z
+
+#/var/lib/docker/volumes/openvpn_26/_data/clients/client-26-01
+
+
+#------------------------------
+#docker run --cap-add=NET_ADMIN \
+#-v openvpn_conf:/opt/Dockovpn \
+#-p 1194:1194/udp -p 80:8080/tcp \
+#-e HOST_ADDR=localhost \
+#--rm \
+#alekslitvinenk/openvpn "$@"
--- scripts/start.sh
+++ scripts/start.sh
@@ -19,16 +19,16 @@ iptables -A FORWARD -i tun0 -j ACCEPT
 iptables -A OUTPUT -o tun0 -j ACCEPT
 
 # Allow forwarding traffic only from the VPN.
-iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -j ACCEPT
+iptables -A FORWARD -i tun0 -o eth0 -s 10.8.26.0/24 -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.26.0/24 -o eth0 -j MASQUERADE
 
 cd "$APP_PERSIST_DIR"
 
 LOCKFILE=.gen
 
-# Regenerate certs only on the first start 
+# Regenerate certs only on the first start
 if [ ! -f $LOCKFILE ]; then
 
     /usr/share/easy-rsa/easyrsa build-ca nopass << EOF
